e76c306fb59d543f335aeb5afeeecbc49fa7757d
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-09-15 23:16:10 +0200

    Updated CombatTab.tsx

--- src/components/CombatTab.tsx
+++ src/components/CombatTab.tsx
@@ -37,7 +37,7 @@ const BowIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
     <path d="M4 20c6-4 6-12 0-16" />
     {/* Corde */}
     <path d="M4 4l8 8L4 20" />
-    {/* Flèche */}
+    {/* Flèches */}
     <path d="M22 2l-8 8" />
     <path d="M22 2l-4 2" />
     <path d="M22 2l-2 4" />
