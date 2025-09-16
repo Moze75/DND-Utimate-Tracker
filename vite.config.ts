72439b71b9da661a85375fff5a1c7301d5381019
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-09-16 09:14:39 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom')
+    }
   },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 });
