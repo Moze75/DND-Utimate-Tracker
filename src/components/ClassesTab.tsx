01b6e239e30c4f2842612726f57fa8d41255e5f4
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-09-23 13:14:08 +0200

    Updated ClassesTab.tsx

--- src/components/ClassesTab.tsx
+++ src/components/ClassesTab.tsx
@@ -18,7 +18,7 @@ import {
   Plus,
   Minus,
   ListChecks,
-  biceps-flexed,
+  biceps,
 } from 'lucide-react';
 
 import toast from 'react-hot-toast';
@@ -1353,7 +1353,7 @@ function ClassResourcesCard({
           items.push(
             <ResourceBlock
               key="innate_sorcery"
-              icon={<biceps-flexed size={20} />}
+              icon={<biceps size={20} />}
               label="Sorcellerie innée"
               total={innateTotal}
               used={innateUsed}
