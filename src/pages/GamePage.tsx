80fa85fdc313355285adcdf6ea91204ab30dc64c
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-09-23 17:19:54 +0200

    Updated GamePage.tsx

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, useCallback, useRef } from 'react';
 import toast from 'react-hot-toast';
 import { LogOut } from 'lucide-react';
+import { SwipeNavigator } from '../components/SwipeNavigator';
 
 import { testConnection } from '../lib/supabase';
 import { Player } from '../types/dnd';
