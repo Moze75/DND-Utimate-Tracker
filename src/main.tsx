e9dffc0f19eaab9dc12375d612a8184f4cb4763e
Moze75 <120105440+Moze75@users.noreply.github.com>
2025-10-04 16:26:43 +0200

    Updated package.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { PWAUpdatePrompt } from './components/PWAUpdatePrompt';
-import { initPWADiagnostic } from './utils/pwaCleanup';
-import { registerPWA } from './utils/pwaRegister';
-
-initPWADiagnostic();
-registerPWA();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <PWAUpdatePrompt />
   </StrictMode>
 );
